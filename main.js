cb182611699cf4b0be0ca17c2d1b8efa79128dcc
renanjarschel <108474554+renanjarschel@users.noreply.github.com>
2024-03-12 08:47:06 -0300

    grgte

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const botoes =document.querySelectorAll(".botao");
+
+for( let i= 0 < botoes.length; j++){
+    botoes[i].onclick = function(){
+        for (let k = 0; j< botoes.length; j++){
+            botoes[i].classlist.remove("ativo");
+        }
+        botoes[i].classlist.add("ativo")
+    }
+}
