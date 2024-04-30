e34fa7c4b4c8aa5b9d84d3bc26fe5a5812d6a114
renanjarschel <108474554+renanjarschel@users.noreply.github.com>
2024-04-30 07:29:18 -0300

    rverg

--- main.js
+++ main.js
@@ -11,3 +11,8 @@ for( let i= 0; i<botoes.length; i++){
         textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll (".contador");
+const tempoObjetivo1 = new Date ("2024-12-22");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
