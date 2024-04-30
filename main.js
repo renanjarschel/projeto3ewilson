53244faa3ac08718b0e63353bc191939dded3c63
renanjarschel <108474554+renanjarschel@users.noreply.github.com>
2024-04-30 07:45:46 -0300

    thrt

--- main.js
+++ main.js
@@ -22,4 +22,18 @@ contadores[0].textContent = calcularTempo(tempoObjetivo1);
 
 function calcularTempo(tempoObjetivo1){
     let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000 );
+    let minutos = Math.floor(segundos / 60 );
+    let horas = Math.floor(minutos / 60 );
+    let dias = Math.floor(horas / 24 );
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias"
+    + horas + "horas"
+    + minutos + "minutos"
+    + segundos + "segundos"
 }
